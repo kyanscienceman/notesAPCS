eb3f2fb032a7e0057ebf3e558f605490f5cebc8b
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-09-19 09:27:35 -0400

    notes 9.19

---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main (String[] args) {
+	Greeter4 g1 = new Greeter4("Hello");
+	System.out.println(g1.greet());
+	Greeter4 g2 = new Greeter4("Hello Sue");
+    }
+}
---
+++ Greeter4.java
@@ -0,0 +1,16 @@
+public class Greeter4 {
+
+    private String greet = "Hello";
+
+    public String greetPerson(String s){
+	return greeting + "" + s;
+    }
+
+    public String void greet() {
+	return greet;
+    }
+}
+
+
+
+
