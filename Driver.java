84d779f48800444b780b48dd7c48535aa5d557b6
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-09-22 09:00:26 -0400

    notes from 9/22

--- Driver.java
+++ Driver.java
@@ -1,7 +1,6 @@
 public class Driver {
-    public static void main (String[] args) {
-	Greeter4 g1 = new Greeter4("Hello");
-	System.out.println(g1.greet());
-	Greeter4 g2 = new Greeter4("Hello Sue");
+    public static void main(String[] args){
+	StringStuff s = new StringStuff();
+	s.test1();
     }
 }
