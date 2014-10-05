7f6d237ae50c80fab4b04de43f689b53db7d534a
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2014-10-05 16:04:33 -0400

    blah

---
+++ greeterPractice/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main(String[] args){
+	Practice p = new Practice();
+	p.setTemp(69);
+	p.getTemp();
+
+	p.setDayOfWeek(2);
+	p.getDayOfWeek();
+
+    }
+}
