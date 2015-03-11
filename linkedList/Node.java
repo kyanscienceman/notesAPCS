f19ac336536edd3cdb87ebf1df29b54948c2a160
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2015-03-11 09:23:58 -0400

    3.11 Notes

---
+++ linkedList/Node.java
@@ -0,0 +1,27 @@
+public class Node{
+
+    /*--------------------- Variables & Contructors -----------------------*/
+    
+    private String data;
+    private Node next;
+
+    public Node(String s){
+	data = s;
+    }
+    public void setData(String s){
+	data = s;
+    }
+    public String getData(){
+	return data;
+    }
+    public void setNext(Node n){
+	next = n;
+    }
+    public Node getNext(){
+	return next;
+    }
+    public String toString(){
+	return data;
+    }
+
+}
